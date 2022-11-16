6a107c6e3d66a586d6ac29facdcfabade627bc5f
ryanm1717 <ry5575@gmail.com>
2022-11-15 23:20:29 -0500

    Testing selenium

--- fanduel_odds.py
+++ fanduel_odds.py
@@ -1,23 +1,29 @@
 # import libraries
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+#from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.chrome.options import Options
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#import time
 
 #changing chromedriver default options
-options = Options()
-options.add_experimental_option("detach", True)
+#options = Options()
+#options.add_experimental_option("detach", True)
 #options.headless = True
 #options.add_argument('window-size=1920x1080') #Headless = True
 
-web = 'https://www.fanduel.com/'
+#web = 'https://sportsbook.fanduel.com/'
 #path = 'C:\Program Files (x86)\chromedriver.exe'
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get(web)
-driver.maximize_window()
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#driver.get(web)
+#driver.maximize_window()
+
+#nfl = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/div/div[1]/div[2]/div/div/div/a[6]').click()
+
+#games = driver.find_elements('xpath', '//*[@id="root"]/div/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div[3]/div/div[3]/div')
+
+import requests

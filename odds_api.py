227dfbbdbfc55847ccfb22c5e4faffb3dbd8035f
ryanm1717 <ry5575@gmail.com>
2022-11-22 16:37:39 -0500

    Have odds for each sportsbook for each game in one big loop

--- odds_api.py
+++ odds_api.py
@@ -38,6 +38,10 @@ date_format = 'iso'
 # - NCAA Basketball: 'basketball_ncaab'
 # - NHL: 'icehockey_nhl'
 
+# Bookmakers:
+# ['sugarhouse', 'bovada', 'betus', 'barstool', 'superbook', 'unibet_us', 'draftkings', 'twinspires', 'williamhill_us', 'fanduel', 'betonlineag', 'betrivers', 
+# # 'lowvig', 'pointsbetus', 'wynnbet', 'circasports', 'mybookieag', 'betmgm', 'betfair', 'gtbets', 'foxbet']
+
 # List of live and upcoming games for the specified sport, along with odds for different bookmakers - deducts from usage quota
 odds_response = requests.get(
     f'https://api.the-odds-api.com/v4/sports/{sport}/odds',
@@ -55,14 +59,83 @@ if odds_response.status_code != 200:
 else:
     odds_json = odds_response.json()
     print('Number of events: ', len(odds_json))
-    print(odds_json[1])
+    print(odds_json[0])
+    
+    for game in odds_json:
+        home_team = game['home_team']
+        away_team = game['away_team']
+
+        bookmaker_odds = {}
+
+        home_team_best_odds = -999999
+        home_team_best_odds_bookmaker_list = []
+        away_team_best_odds = -999999
+        away_team_best_odds_bookmaker_list = []
+        
+        for bookmaker in game['bookmakers']:
+            if home_team == bookmaker['markets'][0]['outcomes'][0]['name']:
+                bookmaker_odds[bookmaker['key']] = {bookmaker['markets'][0]['outcomes'][0]['name']: bookmaker['markets'][0]['outcomes'][0]['price'], bookmaker['markets'][0]['outcomes'][1]['name']: bookmaker['markets'][0]['outcomes'][1]['price']}
+
+                if bookmaker['markets'][0]['outcomes'][0]['price'] == home_team_best_odds:
+                    home_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    #home_team_best_odds = bookmaker['markets'][0]['outcomes'][0]['price']
+
+                if bookmaker['markets'][0]['outcomes'][0]['price'] > home_team_best_odds:
+                    home_team_best_odds_bookmaker_list.clear()
+                    home_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    home_team_best_odds = bookmaker['markets'][0]['outcomes'][0]['price']
+                
+                if bookmaker['markets'][0]['outcomes'][1]['price'] == away_team_best_odds:
+                    away_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    #away_team_best_odds = bookmaker['markets'][0]['outcomes'][1]['price']
+
+                if bookmaker['markets'][0]['outcomes'][1]['price'] > away_team_best_odds:
+                    away_team_best_odds_bookmaker_list.clear()
+                    away_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    away_team_best_odds = bookmaker['markets'][0]['outcomes'][1]['price']
+
+            else:
+                bookmaker_odds[bookmaker['key']] = {bookmaker['markets'][0]['outcomes'][1]['name']: bookmaker['markets'][0]['outcomes'][1]['price'], bookmaker['markets'][0]['outcomes'][0]['name']: bookmaker['markets'][0]['outcomes'][0]['price']}
+
+                if bookmaker['markets'][0]['outcomes'][1]['price'] == home_team_best_odds:
+                    home_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    #home_team_best_odds = bookmaker['markets'][0]['outcomes'][1]['price']
+
+                if bookmaker['markets'][0]['outcomes'][1]['price'] > home_team_best_odds:
+                    home_team_best_odds_bookmaker_list.clear()
+                    home_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    home_team_best_odds = bookmaker['markets'][0]['outcomes'][1]['price']
+                
+                if bookmaker['markets'][0]['outcomes'][0]['price'] == away_team_best_odds:
+                    away_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    #away_team_best_odds = bookmaker['markets'][0]['outcomes'][0]['price']
+
+                if bookmaker['markets'][0]['outcomes'][0]['price'] > away_team_best_odds:
+                    away_team_best_odds_bookmaker_list.clear()
+                    away_team_best_odds_bookmaker_list.append(bookmaker['key'])
+                    away_team_best_odds = bookmaker['markets'][0]['outcomes'][0]['price']
+        
+        print('Matchup: ' + home_team + ' vs. ' + away_team + ' - Odds below:')
+        if len(home_team_best_odds_bookmaker_list) > 1 and len(away_team_best_odds_bookmaker_list) > 1:
+            print('Best odds for ' + home_team + ': ' + str(home_team_best_odds) + ' at ' + str(home_team_best_odds_bookmaker_list))
+            print('Best odds for ' + away_team + ': ' + str(away_team_best_odds) + ' at ' + str(away_team_best_odds_bookmaker_list))
+        elif len(home_team_best_odds_bookmaker_list) == 1 and len(away_team_best_odds_bookmaker_list) > 1:
+            print('Best odds for ' + home_team + ': ' + str(home_team_best_odds) + ' at ' + home_team_best_odds_bookmaker_list[0])
+            print('Best odds for ' + away_team + ': ' + str(away_team_best_odds) + ' at ' + str(away_team_best_odds_bookmaker_list))
+        elif len(home_team_best_odds_bookmaker_list) > 1 and len(away_team_best_odds_bookmaker_list) == 1:
+            print('Best odds for ' + home_team + ': ' + str(home_team_best_odds) + ' at ' + str(home_team_best_odds_bookmaker_list))
+            print('Best odds for ' + away_team + ': ' + str(away_team_best_odds) + ' at ' + away_team_best_odds_bookmaker_list[0])
+        else:
+            print('Best odds for ' + home_team + ': ' + str(home_team_best_odds) + ' at ' + home_team_best_odds_bookmaker_list[0])
+            print('Best odds for ' + away_team + ': ' + str(away_team_best_odds) + ' at ' + away_team_best_odds_bookmaker_list[0])            
+        print(bookmaker_odds)
+        print('')
 
     # Check the usage quota
     print('Remaining requests: ', odds_response.headers['x-requests-remaining'])
     print('Used requests: ', odds_response.headers['x-requests-used'])
 
-# Bookmakers
-# barstool, betmgm, betonlineag, betfair, foxbet, lowvig, circasports, superbook, draftkings, fanduel, williamhill_us, betus, twinspires, unibet_us, sugarhouse, betrivers, gtbets, wynnbet, 
+
 
 
 

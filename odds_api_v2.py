19a4cc8a233c6f9d5188795fdf16649a97779e58
ryanm1717 <ry5575@gmail.com>
2022-11-22 22:53:35 -0500

    Classes working with API

--- odds_api_v2.py
+++ odds_api_v2.py
@@ -43,30 +43,49 @@ date_format = 'iso'
 # # 'lowvig', 'pointsbetus', 'wynnbet', 'circasports', 'mybookieag', 'betmgm', 'betfair', 'gtbets', 'foxbet']
 
 class Game:
-    def __init__(self, game_id, sport_key, sport_title, commence_time, home_team, away_team, h2h_home_sportsbook, h2h_away_sportsbook, h2h_home_odds, h2h_away_odds):
+    def __init__(self, game_id, sport_key, sport_title, commence_time, home_team, away_team, h2h_sportsbooks, h2h_home_odds, h2h_away_odds):
         self.game_id = game_id
         self.sport_key = sport_key
         self.sport_title = sport_title
         self.commence_time = commence_time
         self.home_team = home_team
         self.away_team = away_team
-        self.h2h_home_sportsbook = h2h_home_sportsbook
-        self.h2h_away_sportsbook = h2h_away_sportsbook
+        self.h2h_sportsbooks = h2h_sportsbooks
         self.h2h_home_odds = h2h_home_odds
         self.h2h_away_odds = h2h_away_odds
 
     def best_odds(self):
         h2h_home_best_odds = -999999
         h2h_home_worst_odds = 999999
+        h2h_home_best_odds_sportsbook_list = []
 
         h2h_away_best_odds = -999999
         h2h_away_worst_odds = 999999
+        h2h_away_best_odds_sportsbook_list = []
 
         for i in range(0, len(self.h2h_home_odds)):
-            #if self.h2h_home_odds[i]
+            if self.h2h_home_odds[i] == h2h_home_best_odds:
+                h2h_home_best_odds_sportsbook_list.append(self.h2h_sportsbooks[i])
 
+            if self.h2h_home_odds[i] > h2h_home_best_odds:
+                h2h_home_best_odds_sportsbook_list.clear()
+                h2h_home_best_odds_sportsbook_list.append(self.h2h_sportsbooks[i])
+                h2h_home_best_odds = self.h2h_home_odds[i]
 
+            if self.h2h_away_odds[i] == h2h_away_best_odds:
+                h2h_away_best_odds_sportsbook_list.append(self.h2h_sportsbooks[i])
 
+            if self.h2h_away_odds[i] > h2h_away_best_odds:
+                h2h_away_best_odds_sportsbook_list.clear()
+                h2h_away_best_odds_sportsbook_list.append(self.h2h_sportsbooks[i])
+                h2h_away_best_odds = self.h2h_away_odds[i]
+        
+        matchup = 'Matchup between ' + self.home_team + ' vs. ' + self.away_team + ' - odds below\n' 
+        home_odds = 'Best odds for ' + self.home_team + ': ' + str(h2h_home_best_odds) + ' at ' + str(h2h_home_best_odds_sportsbook_list) + '\n'
+        away_odds = 'Best odds for ' + self.away_team + ': ' + str(h2h_away_best_odds) + ' at ' + str(h2h_away_best_odds_sportsbook_list) + '\n'
+
+        return matchup + home_odds + away_odds
+        
 
 # List of live and upcoming games for the specified sport, along with odds for different bookmakers - deducts from usage quota
 odds_response = requests.get(
@@ -82,10 +101,48 @@ odds_response = requests.get(
 
 if odds_response.status_code != 200:
     print(f'Failed to get odds: status code {odds_response.status_code}, response body {odds_response.text}')
+    
 else:
     odds_json = odds_response.json()
-    print('Number of events: ', len(odds_json))
+    print('Number of events: ', len(odds_json), '\n')
+
+    g_id = 1
+    game_class = {}
+    for game in odds_json:
+
+        home_team = game['home_team']
+        away_team = game['away_team']
+
+        h2h_sportsbook = []
+        h2h_home_odds = []
+        h2h_away_odds = []
+
+        for bookmaker in game['bookmakers']:
+            h2h_sportsbook.append(bookmaker['key'])
+
+            if home_team == bookmaker['markets'][0]['outcomes'][0]['name']:
+                h2h_home_odds.append(bookmaker['markets'][0]['outcomes'][0]['price'])
+                h2h_away_odds.append(bookmaker['markets'][0]['outcomes'][1]['price'])
+            else:
+                h2h_home_odds.append(bookmaker['markets'][0]['outcomes'][1]['price'])
+                h2h_away_odds.append(bookmaker['markets'][0]['outcomes'][0]['price'])
+
+        game_class['game_' + str(g_id)] = Game(game_id=game['id'], sport_key=game['sport_key'], sport_title=game['sport_title'], commence_time=game['commence_time'], home_team=game['home_team'], away_team=game['away_team'], h2h_sportsbooks=h2h_sportsbook, h2h_home_odds=h2h_home_odds, h2h_away_odds=h2h_away_odds)
+
+        g_id += 1
+
+    #print(game_class['game_1'].best_odds())
+    #print(game_class['game_1'].h2h_sportsbooks)
+    #print(game_class['game_1'].h2h_home_odds)
+    #print(game_class['game_1'].h2h_away_odds)
+    
+    for x in game_class:
+        print(game_class[x].best_odds())
 
     # Check the usage quota
     print('Remaining requests: ', odds_response.headers['x-requests-remaining'])
     print('Used requests: ', odds_response.headers['x-requests-used'])
+
+    
+
+
